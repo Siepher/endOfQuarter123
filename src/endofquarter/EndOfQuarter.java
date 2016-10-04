e947b38b7df12f6882c4a2e60f9090b73b071e67
student <student@172.26.3.60>
2016-10-04 14:04:25 -0600

    Forrest

---
+++ src/endofquarter/EndOfQuarter.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package endofquarter;
+
+/**
+ *
+ * @author student
+ */
+public class EndOfQuarter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 6; i++)
+          System.out.println("Enter number: "+i);
+    
+        int i = 10;
+    
+    }
+    
+}
