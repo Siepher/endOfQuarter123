626b29ddfe3092a4dd5596afe2457e81efcd42a4
student <student@172.26.2.251>
2016-10-05 14:03:41 -0600

    Forrest

--- src/endofquarter/MathFunctions.java
+++ src/endofquarter/MathFunctions.java
@@ -3,9 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-        
-
-
 package endofquarter;
 
 /**
@@ -13,8 +10,17 @@ package endofquarter;
  * @author student
  */
 public class MathFunctions {
-    int e = 6;
-        double root = MathFunctions.sqrt(e);
-    double absolute = Math.abs(e); 
+    
+
+
+    
+   
+public static void main(String args[]) {
+      double x = 6;
+
+      System.out.printf("The value of e is %.4f%n", Math.E);
+      System.out.printf("sqrt(%.3f) is %.3f%n", x, Math.sqrt(x));
+   }
+    
     
 }
